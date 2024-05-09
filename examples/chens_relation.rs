463b11300fd69a021c1ef8378da389e022855feb
ladezai <83292183+ladezai@users.noreply.github.com>
2024-05-09 17:06:12 +0200

    updated and corrected example

--- examples/chens_relation.rs
+++ examples/chens_relation.rs
@@ -22,25 +22,26 @@ pub fn impure_2() -> Result<TruncatedSignature<f32>, TruncatedSignatureError> {
     Ok(sig1)
 }
 
+pub fn check_distance_l1(sig1 : TruncatedSignature<f32>, sig2 : TruncatedSignature<f32>) -> f32 {
+    (sig1 - sig2).unwrap().norm_l1()
+}
+
+
 pub fn main() -> () {
     println!("In this example we consider the simple path W_s = (2s, s).");
     println!("We consider the points W_0 = (0,0), W_1 = (2,1), W_3=(6,3).");
     println!("By Chen's relation S(W_[0,1]) \\otimes S(W_[1,3]) = S(W_[0,3])");
     println!("Therefore, we compute the signature using the LHS in the first step...");
 
-    let res = impure_1(); 
-    match res {
-        Ok(sig1) => println!("{:?}", &sig1),
-        _ => println!("An error occured!")
-    };
+    let sig1 = impure_1().unwrap(); 
+    println!("{:?}", &sig1);
 
     println!("And using the RHS, we get the same result: ");
-    let res = impure_1(); 
-    match res {
-        Ok(sig1) => println!("{:?}", &sig1),
-        _ => println!("An error occured!")
-    };
+    let sig2 = impure_2().unwrap(); 
+    println!("{:?}", &sig2);
+    println!("Chen's relation seem to hold...");
 
-    println!("Chen's relation seem to hold!")
+    let l1_dst = check_distance_l1(sig1, sig2);
+    println!("The difference in l1-norm is: {:?}", l1_dst);
 }
 
